458d288edf48989f0f016d541b99a1bc6d858aa8
Akanksha Tanwar <55976305+akankshatanwar1701@users.noreply.github.com>
2020-07-21 23:31:01 +0530

    Adding n

--- tictactoe/conversion.py
+++ tictactoe/conversion.py
@@ -1,12 +1,12 @@
-def onetotwod(arr):
-	board=[[0 for x in range(3)] for y in range(3)]
+def onetotwod(arr,n):
+	board=[[0 for x in range(n)] for y in range(n)]
 	k=0
-	for i in range(3):
-		for j in range(3):
+	for i in range(n):
+		for j in range(n):
 			board[i][j]=arr[k]
 			k=k+1
 	return board
 
-def twotooned(row,col):
-	index= col+3*row
+def twotooned(row,col,n):
+	index= col+n*row
 	return index
