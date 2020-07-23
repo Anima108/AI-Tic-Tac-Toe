d3ba47b8f13039c98dd7adb1e8c16285b7601823
Akanksha Tanwar <55976305+akankshatanwar1701@users.noreply.github.com>
2020-07-23 17:19:16 +0530

    comments

--- tictactoe/conversion.py
+++ tictactoe/conversion.py
@@ -1,3 +1,4 @@
+#Django sends 1-D array with 9 elements, which is to be converted into a 2-D board
 def onetotwod(arr,n):
 	board=[[0 for x in range(n)] for y in range(n)]
 	k=0
@@ -7,6 +8,9 @@ def onetotwod(arr,n):
 			k=k+1
 	return board
 
+#sending one single index number back as the bestmove 
+#given i,j we can know which block is represented by them in 1-D array
+# 0,0 being 0.......2,2 being 8th block
 def twotooned(row,col,n):
 	index= col+n*row
 	return index
